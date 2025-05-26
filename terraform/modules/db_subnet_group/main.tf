06f16b0fc75de35bf201b5afc9cd43a1c8aeff6f
dev-kt07 <147971609+dev-kt07@users.noreply.github.com>
2025-05-26 17:41:49 +0530

    Update main.tf

--- terraform/modules/db_subnet_group/main.tf
+++ terraform/modules/db_subnet_group/main.tf
@@ -1,7 +1,7 @@
 resource "aws_db_subnet_group" "this" {
   name       = var.name
   description = var.description
-  subnet_ids = var.subnet_ids
+  subnet_ids = ["subnet-04ad28a69f0c1e0a0", "subnet-0cf36861e446930a3"]
 
   tags = var.tags
 }
